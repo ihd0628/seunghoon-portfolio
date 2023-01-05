d2a63dd3c483a19d8859331e46d7c21c48453f12
Hoon <si932174@gmail.com>
2023-01-05 20:29:34 +0900

    remove : 초기 세팅(iindex.js 만 남기고 삭제)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    "airbnb", // or airbnb-base
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended", // 설치 한경우
-    "plugin:import/errors", // 설치한 경우
-    "plugin:import/warnings", // 설치한 경우
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'airbnb', // or airbnb-base
+    'plugin:react/recommended',
+    // 'plugin:jsx-a11y/recommended', // 설치 한경우
+    // 'plugin:import/errors', // 설치한 경우
+    // 'plugin:import/warnings', // 설치한 경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "linebreak-style": true,
-    "import/prefer-default-export": true,
-    "import/extensions": true,
-    "no-use-before-define": true,
-    "import/no-unresolved": true,
-    "react/react-in-jsx-scope": true,
-    "import/no-extraneous-dependencies": true, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
-    "no-shadow": true,
-    "react/prop-types": true,
-    "react/jsx-filename-extension": [
+    'linebreak-style': 1,
+    'import/prefer-default-export': 1,
+    'import/extensions': 1,
+    'no-use-before-define': 1,
+    'import/no-unresolved': 1,
+    'react/react-in-jsx-scope': 1,
+    'import/no-extraneous-dependencies': 1, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'no-shadow': 1,
+    'react/prop-types': 1,
+    'react/jsx-filename-extension': [
       2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    "jsx-a11y/no-noninteractive-element-interactions": true,
-    "@typescript-eslint/explicit-module-boundary-types": true,
+    'jsx-a11y/no-noninteractive-element-interactions': 1,
+    '@typescript-eslint/explicit-module-boundary-types': 1,
   },
+  // quotes: [2, 'single'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },

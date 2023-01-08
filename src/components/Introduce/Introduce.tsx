f40b6d07e9003779153c42ac38cc6e14ab68c1e3
Hoon <si932174@gmail.com>
2023-01-08 16:29:15 +0900

    design : Introduce 화면 제작

--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -5,7 +5,22 @@ import backGround from './images/IntroduceBackground.jpg';
 function Introduce() {
   return (
     <IntroduceContainer>
-      <Head>프론트 엔드 개발자 이승훈</Head>
+      <Head>안녕하세요.</Head>
+      <br />
+      <div>
+        <Name>이승훈</Name>
+        <Head>입니다.</Head>
+      </div>
+      <br />
+      <br />
+      <br />
+      <Description>프론트엔드 개발자를 향해 달려가는 중입니다.</Description>
+      <br />
+      <Description>배우고 성장하여 도움을 주는 것을 좋아합니다.</Description>
+      <br />
+      <Description>
+        기록의 중요성을 알고 생각을 정리하여 기록하기 위해 노력합니다.
+      </Description>
     </IntroduceContainer>
   );
 }
@@ -19,7 +34,8 @@ const IntroduceContainer = styled.section`
     content: '';
     background-image: url(${backGround});
     background-size: cover;
-    opacity: 0.5;
+    z-index: -1;
+    opacity: 0.45;
     position: absolute;
     top: 0px;
     left: 0px;
@@ -28,9 +44,19 @@ const IntroduceContainer = styled.section`
   }
 `;
 
-const Head = styled.h1`
+const Head = styled.span`
   font-size: 65px;
+  font-weight: 600;
+`;
+
+const Name = styled.span`
+  font-size: 90px;
   font-weight: 900;
+`;
+
+const Description = styled.div`
+  font-size: 35px;
+  font-weight: 400;
 `;
 
 export default Introduce;
